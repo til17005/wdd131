481092ce05bccb8cce473575611af621aa5c7174
til17005 <matthew_tilton@outlook.com>
2025-05-12 14:37:23 -0600

    BoM Fixed

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -11,16 +11,15 @@ const button = document.querySelector('button');
 // list - ul element in whcih an li will be added with the favorit BoM chapter
 const list = document.querySelector('#list');
 
-// li - element to be place inside the list (ul)
-const li = document.createElement('li');
-// delButton - a button with an X icon for deleting an entry from the BoM favorite chapters
-const delButton = document.createElement('button');
-
-
 // Click event added to button
 button.addEventListener("click", function () {
     // trim removes white space from the front and back of the text. trimStart() or trimEnd() could also be used to trim the front and back of the text.
     if (input.value.trim() !== '') {
+        // li - element to be place inside the list (ul)
+        const li = document.createElement('li');
+        // delButton - a button with an X icon for deleting an entry from the BoM favorite chapters
+        const delButton = document.createElement('button');
+        
         // This will populate the li with the input value
         li.textContent = input.value;
         // This creates a visual Big Red X for removing (deleting) an entry
@@ -32,13 +31,15 @@ button.addEventListener("click", function () {
         li.append(delButton);
         // This will append or add the li to the ul (list)
         list.append(li);
+
+        delButton.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        });
     }
-})
-
-delButton.addEventListener("click", function () {
-    list.removeChild(li);
+    input.value = '';
     input.focus();
-})
+});
 
-input.value = '';
-input.focus();
+
+
